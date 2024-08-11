4b353506b4c8892f9cd46fda38c9adccad1715db
Tahmid Washy <tahmidwashy@Tahmids-Laptop.local>
2024-08-11 06:25:41 -0400

    Initial Commit

---
+++ PrayerTimesWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrayerTimesWidget
+//
+//  Created by Tahmid Washy on 8/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrayerTimesWidget/PrayerTimesWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerTimesWidgetApp.swift
+//  PrayerTimesWidget
+//
+//  Created by Tahmid Washy on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PrayerTimesWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
